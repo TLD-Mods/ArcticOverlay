8d93e3f8a38ecf14101e71ca2701ce9c39bf7c55
Digitalzombie <blackdevil32@gmx.net>
2024-07-16 19:20:45 +0200

    Add project files.

---
+++ ArcticOverlay.cs
@@ -0,0 +1,29 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Il2CppInterop;
+using Il2CppInterop.Runtime.Injection; 
+using System.Collections;
+
+namespace ArcticOverlay
+{
+	public class ArcticOverlay : MelonMod
+	{
+		public override void OnInitializeMelon()
+		{           
+			MelonLogger.Msg("Mod started!");
+        }
+
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+       
+
+
+        }
+
+        public override void OnUpdate()
+		{
+
+		}
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using MelonLoader;
+using ArcticOverlay;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArcticOverlay")]
+[assembly: AssemblyCopyright("STBlade, Digitalzombie")]
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: MelonInfo(typeof(ArcticOverlay.ArcticOverlay), "ArcticOverlay", "0.5.0", "STBlade, Digitalzombie", null)]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
